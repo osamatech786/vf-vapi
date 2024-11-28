2c58b06024950c09ccca6a8ed8477240a78fde19
Ahmed <vactum0@gmail.com>
2024-11-28 19:34:58 +0000

    add get router

--- src/router.js
+++ src/router.js
@@ -5,4 +5,8 @@ const router = Router();
 
 router.post('/chat/completions', customLLMHandler.dmAPI);
 
+router.get('/chat/completions', (req, res) => {
+    res.send({ message: 'GET is working, but this is a POST route.' });
+});
+
 export { router };
